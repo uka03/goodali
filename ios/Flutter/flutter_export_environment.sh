4263168b6401de3f954e3c5707541558f55b1123
Khaliun1 <80507498+Khaliun1@users.noreply.github.com>
2022-09-01 14:16:16 +0800

    h
    h

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/z/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/z/Documents/sideProj/goodali"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/z/Documents/sideProj/goodali/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/z/Documents/sideProj/goodali/.dart_tool/package_config.json"
